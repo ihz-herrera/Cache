18619aa4522bf91a6354f157b2cd567339010821
Ivan Herrera <ivan.herrera@techsoft.com.mx>
2024-05-24 10:09:01 -0700

    Agregar archivos de proyecto.

---
+++ Cache/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cache
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Iniciando aplicación...");
+
+            //crear un ciclo para ejecutar la validación de la licencia cada 1 segundo
+            while (true)
+            {
+                //Medir el tiempo de cada ejecución
+                var tiempoInicio = DateTime.Now;
+
+                //crear una instancia de LicenciasService
+                var licenciasService = new Repository.LicenciasService();
+
+
+                var resultado = licenciasService.LicenciaValida();
+
+                var tiempoFin = DateTime.Now;
+                var tiempoTranscurrido = tiempoFin - tiempoInicio;
+
+                //validar la licencia
+                if (licenciasService.LicenciaValida())
+                {
+                    
+                    Console.WriteLine($"La licencia es válida.{tiempoTranscurrido}");
+                }
+                else
+                {
+                    Console.WriteLine($"La licencia ha vencido.{tiempoTranscurrido}");
+                }
+
+                //esperar 1 segundo
+                System.Threading.Thread.Sleep(1000);
+                tiempoFin = DateTime.Now;
+            }
+        }
+    }
+}
---
+++ Cache/Repository/LicenciasService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using Cache.Modelos;
+
+
+namespace Cache.Repository
+{
+    internal class LicenciasService
+    {
+        private MemoryCache memoryCache;
+        private CacheItemPolicy policy;
+
+
+        public LicenciasService()
+        {
+            InitializeCache();
+        }
+
+        public void InitializeCache()
+        {
+            memoryCache = MemoryCache.Default;
+            policy = new CacheItemPolicy();
+            policy.AbsoluteExpiration = DateTimeOffset.Now.AddSeconds(10);
+        }
+
+        public bool LicenciaValida()
+        {
+            string cacheKey = "licencia";
+
+
+            Licencia licencia;
+
+            // Comprobar si los datos ya están en caché
+            if (memoryCache.Contains(cacheKey))
+            {
+                licencia = memoryCache.Get(cacheKey) as Licencia;
+            }
+            else
+            {
+                // Simular una operación costosa para obtener datos
+                licencia = ConsultarLicencia();
+                memoryCache.Add(cacheKey, licencia, policy);
+                
+            }
+
+            // Comprobar si la licencia está vencida
+            return  DateTime.Parse(licencia.FechaVencimiento) < DateTime.Now ? false : true;
+
+            
+        }
+
+
+        private Licencia ConsultarLicencia()
+        {
+            // Simulación de obtención de datos del servicio
+            System.Threading.Thread.Sleep(3000); // Simula un retraso
+            return new Licencia
+            {
+                Serial = "123456",
+                Caracteristicas = "Licencia de prueba",
+                FechaVencimiento = "2021-12-31"
+            };
+        }
+    }
+}
